108bc2c5f773b0a0e143da0f03028012f281c706
Jason Harvey <alienth@gmail.com>
2020-02-08 15:49:26 -0900

    terraform: Change prompt info.

--- plugins/terraform/terraform.plugin.zsh
+++ plugins/terraform/terraform.plugin.zsh
@@ -4,7 +4,13 @@ function tf_prompt_info() {
     # check if in terraform dir
     if [[ -d .terraform && -r .terraform/environment  ]]; then
       workspace=$(cat .terraform/environment) || return
-      echo "[${workspace}]"
+      if [[ "$workspace" == "default" ]]; then
+        echo -n $ZSH_THEME_TF_PROMPT_PREFIX_DEFAULT
+      else
+        echo -n $ZSH_THEME_TF_PROMPT_PREFIX_NONDEFAULT
+      fi
+      echo -n "${workspace}"
+      echo -n $ZSH_THEME_TF_PROMPT_SUFFIX
     fi
 }
 
